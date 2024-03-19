d08333a475ac498b7ea562e078815044098aa781
AVANZA\Manzoor.Hussain <manzoor.hussain@avanazsolutions.com>
2024-03-19 11:09:10 +0500

    Euerka Server

---
+++ euerka-server/src/test/java/com/ecommerence/euerka/server/EuerkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerence.euerka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuerkaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
